bd499bba7d68081c702e756af7ab81baf63bc44f
sunildipun <sunil.6318ie@gmail.com>
2020-12-27 19:09:06 +0530

    cards with images and title

--- src/components/cards/cards.component.jsx
+++ src/components/cards/cards.component.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './cards.style.css';
 
-export default function Cards() {
+export default function Cards({id, image, info, name, price}) {
     return (
-        <div>
-            Cards
+        <div className="cards">
+            <img className="image" src={image} alt="name"/>
+            <div className="content">
+                <div className="tour-mains">
+                    <h4>{name}</h4>
+                    <h4 className="tour-price">{price}$</h4>
+                </div>
+            </div>
         </div>
     )
 }
--- src/page/home/home.component.jsx
+++ src/page/home/home.component.jsx
@@ -5,13 +5,30 @@ import Cards from './../../components/cards/cards.component';
 
 class Home extends Component {
 
+    constructor() {
+        super();
 
+        this.state = {
+            toursList: [],
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://course-api.com/react-tours-project')
+            .then(response => response.json())
+            .then(list => this.setState({toursList: list}));
+    }
 
     render() {
         return (
             <div className="home">
                 <div className="title">
                     <h1 className="head">Our Tours</h1>
+                    {
+                        this.state.toursList.map(({id, ...otherProperty}) => (
+                            <Cards key={id} {...otherProperty}/>
+                        ))
+                    }
                 </div>
                 
             </div>

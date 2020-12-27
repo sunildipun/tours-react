f611b9f3408253d29a3599adb60fe0f33821343a
sunildipun <sunil.6318ie@gmail.com>
2020-12-27 20:55:38 +0530

    usestate to hide and show paragraph

--- src/components/cards/cards.component.jsx
+++ src/components/cards/cards.component.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './cards.style.css';
 
 export default function Cards({id, image, info, name, price}) {
+    const [readMore, setReadMore] = useState(false);
     return (
         <div className="cards">
             <img className="image" src={image} alt="name"/>
@@ -10,6 +11,12 @@ export default function Cards({id, image, info, name, price}) {
                     <h4>{name}</h4>
                     <h4 className="tour-price">{price}$</h4>
                 </div>
+                <p className="tour-info">
+                    {readMore ? info : `${info.substring(0,200)}...`}
+                    <button onClick={() => setReadMore(!readMore)}>
+                        {readMore ? 'show less' : 'read more'}
+                    </button>
+                </p>
             </div>
         </div>
     )
